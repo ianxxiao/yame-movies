8044a6c70ebb7eea732493ccdd5d82d0a8b61df6
Ian Xiao <ian.xxiao@gmail.com>
2020-01-15 15:11:10 -0500

    - initial commit - added base files

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: sh setup.sh && streamlit run recommendation_engine_app.py
---
+++ recommendation_engine_app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+
+def main():
+    st.title("Recommendation System")
+    st.subheader("Find the Best Movie")
+
+
+if __name__ == '__main__':
+    main()
