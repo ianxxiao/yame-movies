7531a1e657377fddb2333c8d5cd426f1eb38b473
Ian Xiao <ian.xxiao@gmail.com>
2020-01-16 13:17:17 -0500

    - finish genre selection function - add try except block in case of not enough sample

--- app.py
+++ app.py
@@ -7,6 +7,13 @@ from functions.recommendation import find_recommendation
 from config import MINI_DATASET_URL
 
 
+def isin_genres(df, selected_genres):
+
+    df = df.apply(lambda x: 1 if (x <= selected_genres) else 0)
+
+    return df
+
+
 def cast_int(value):
     try:
         value = int(value)
@@ -16,18 +23,18 @@ def cast_int(value):
 
 
 def process_data(links, movies, ratings):
-
     # Process Movie Data
     movies['year'] = movies['title'].apply(lambda x: x.split('(')[-1]
                                            .strip(' )')).apply(lambda x: cast_int(x))
     movies['genres'] = movies['genres'].replace("(no genres listed)", None)
+
+    movies['genres_set'] = movies['genres'].apply(lambda x: set(x.split("|")))
 
     return links, movies, ratings
 
 
 @st.cache
 def get_data():
-
     # Create directory and downlaod data if not exist
     if not os.path.isfile('./data/mini_dataset.zip'):
         os.mkdir('./data')
@@ -38,21 +45,16 @@ def get_data():
     movies = pd.read_csv(ZipFile('./data/mini_dataset.zip').open('ml-latest-small/movies.csv'))
     ratings = pd.read_csv(ZipFile('./data/mini_dataset.zip').open('ml-latest-small/ratings.csv'))
 
-    # Process data for display
-    links, movies, ratings = process_data(links, movies, ratings)
-
     return links, movies, ratings
 
 
 @st.cache
 def get_min_max_year(year):
-
     return [int(year.min()), int(year.max())]
 
 
 @st.cache
 def get_genre_set(genres):
-
     genre_list = []
 
     # Add individual genre value to to genre list
@@ -65,9 +67,9 @@ def get_genre_set(genres):
 
 
 def main():
-
     # Load Data
     links, movies, ratings = get_data()
+    p_links, p_movies, p_rating = process_data(links.copy(), movies.copy(), ratings.copy())
 
     # Set Up the Layout
     st.title("Yet Another Movie Recommender")
@@ -76,19 +78,38 @@ def main():
                                          (5, 10, 20))
 
     add_year_selector = st.sidebar.slider(label="Select Year Range",
-                                          min_value=get_min_max_year(movies.year)[0],
-                                          max_value=get_min_max_year(movies.year)[1],
-                                          value=(get_min_max_year(movies.year)[0] + 20,
-                                                 get_min_max_year(movies.year)[1] - 20),
+                                          min_value=get_min_max_year(p_movies.year)[0],
+                                          max_value=get_min_max_year(p_movies.year)[1],
+                                          value=(get_min_max_year(p_movies.year)[0] + 20,
+                                                 get_min_max_year(p_movies.year)[1] - 20),
                                           step=1)
 
-    add_genre_selector = st.sidebar.multiselect(label="select the Genre (default to Any)",
-                                                options=get_genre_set(movies.genres))
+    add_genre_selector = st.sidebar.multiselect(label="Select the Genre (default to any)",
+                                                options=get_genre_set(p_movies.genres))
 
     # Display Data
     st.subheader(f"Here are {add_selectbox} movies between {add_year_selector[0]} and {add_year_selector[1]}")
-    st.table(movies[["title", "genres"]].loc[(movies['year'] >= add_year_selector[0]) &
-                                             (movies['year'] <= add_year_selector[1])].sample(int(add_selectbox)))
+    if not add_genre_selector:
+        try:
+            st.table(p_movies[["title", "genres"]].loc[(p_movies['year'] >= add_year_selector[0]) &
+                                                     (p_movies['year'] <= add_year_selector[1])]
+                                                .sample(int(add_selectbox)))
+        except ValueError:
+            st.write("Not enough movies, here are all.")
+            st.table(p_movies[["title", "genres"]].loc[(p_movies['year'] >= add_year_selector[0]) &
+                                                     (p_movies['year'] <= add_year_selector[1])])
+
+    else:
+        try:
+            st.table(movies[["title", "genres"]].loc[(p_movies['year'] >= add_year_selector[0]) &
+                                                     (p_movies['year'] <= add_year_selector[1]) &
+                                                     (isin_genres(p_movies['genres_set'], set(add_genre_selector)))]
+                                                .sample(int(add_selectbox)))
+        except ValueError:
+            st.write("Not enough movies, here are all.")
+            st.table(movies[["title", "genres"]].loc[(p_movies['year'] >= add_year_selector[0]) &
+                                                     (p_movies['year'] <= add_year_selector[1]) &
+                                                     (isin_genres(p_movies['genres_set'], set(add_genre_selector)))])
 
 
 if __name__ == '__main__':
